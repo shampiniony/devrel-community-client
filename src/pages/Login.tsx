5a28058cc6aa7294ccf93226bc5d513cbebc3268
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2023-12-16 00:11:50 +0300

    removed stupid eval

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,13 @@
 import { useAuthStore } from './../store.ts';
 
 export const Login = () => {
-  const login = useAuthStore((state: { login: any }) => state.login);
+  const login = useAuthStore((state: { login: any }) => state.login); 
 
   return (
     <>
       <h1
         onClick={() => {
-          // login("vania", "vania2010");
-          eval("console.log('hello!')")
+          login("vania", "vania2010");
         }}
       >
         Login
