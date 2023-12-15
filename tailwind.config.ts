e984edb2a5f9d733ec86fff651707ef3fad5304b
mityaiii <pericle04@mail.ru>
2023-12-16 00:32:08 +0300

    update configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,29 @@
-import type { Config } from 'tailwindcss'
+import colors, { lime } from "tailwindcss/colors";
+import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { ...colors.lime, DEFAULT: lime[600] },
+        "text-primary": {
+          DEFAULT: "#03045E",
+        },
+        "second-primary": {
+          DEFAULT: "#e5e7eb",
+        },
+        detail: {
+          DEFAULT: "#474306",
+        },
+      },
+      animation: {
+        "spin-slow": "spin 10s linear infinite",
+      },
+    },
+    fontFamily: {
+      forNumbers: ["Graduate", "sans-serif"],
+    },
   },
   plugins: [],
-} satisfies Config
-
+} satisfies Config;
