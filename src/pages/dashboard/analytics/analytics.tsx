e2a4a9c076f48b11ef6958531cbb5c93c96248cc
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2023-12-17 11:54:27 +0300

    added plugins

--- src/pages/dashboard/analytics/analytics.tsx
+++ src/pages/dashboard/analytics/analytics.tsx
@@ -1,18 +1,20 @@
 import { SmartUrl } from '../components/smarturl';
 import { Cities } from './analytics.cities';
 import { GptModel } from './analytics.gpt';
+import { Plugins } from './analytics.plugins';
 import { HotUrls } from './anayltics.hoturls';
 
 export const Analytics = () => {
   return (
     <div className='flex w-full h-full gap-5 pb-10'>
-      <div className='flex flex-col h-full w-5/12 overflow-y-scroll no-scrollbar gap-5'>
+      <div className='flex flex-col h-full w-5/12 gap-5'>
         <Cities />
         <div className='h-1/2 w-full bg-second-primary rounded-xl p-5'>
           <h1 className='font-bold text-3xl'>plugins</h1>
+          <Plugins/>
         </div>
       </div>
-      <div className='flex flex-col h-full w-7/12 overflow-y-scroll no-scrollbar gap-5'>
+      <div className='flex flex-col h-full w-7/12 gap-5'>
         <div className='flex flex-row h-3/5 w-full gap-5'>
           <div className='w-1/2 bg-second-primary rounded-xl p-5'>
             <h1 className='font-bold text-3xl'>last event</h1>
